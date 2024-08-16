39f24a7f72c50351a710bd36d8000309583e5282
Beginercod <Beginercod@users.noreply.github.com>
2024-08-16 23:18:18 +0200

    Changed input mapping

--- Player.gd
+++ Player.gd
@@ -3,13 +3,13 @@ const SPEED = 300.0
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionx = Input.get_axis("a", "d")
+	var directionx = Input.get_axis("ui_left", "ui_right")
 	if directionx:
 		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	var directiony = Input.get_axis("w", "s")
+	var directiony = Input.get_axis("ui_up", "ui_down")
 	if directiony:
 		velocity.y = directiony * SPEED
 	else:
