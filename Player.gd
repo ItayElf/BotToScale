4d99645f38c09b7d4bd0378cf93b29ca36c379e1
ItayElf <thh123321@gmail.com>
2024-08-17 09:44:26 +0300

    Fix: refactor code

--- Player.gd
+++ Player.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
-const SPEED = 300.0
-func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionx = Input.get_axis("ui_left", "ui_right")
-	if directionx:
-		velocity.x = directionx * SPEED
+
+@export var speed: float = 300.0
+
+## Sets the velocity from the user input
+func _change_velocity():
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = direction_x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 	
-	var directiony = Input.get_axis("ui_up", "ui_down")
-	if directiony:
-		velocity.y = directiony * SPEED
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = direction_y * speed
 	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.x, 0, speed)
+
+func _physics_process(delta):
+	_change_velocity()
 	move_and_slide()
