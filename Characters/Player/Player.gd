48bff9cf44658fcb912f1059a96c43ba61b0e563
endevour97 <thegreatendeavour97@gmail.com>
2024-08-17 09:56:31 +0200

    Fixed faster diagonal move speed and added acceleration

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,21 +1,22 @@
 extends CharacterBody2D
 
 @export var speed: float = 300.0
+@export var acceleration: float = 1300.0
+@export var deceleration: float = 1500.0
 
 ## Sets the velocity from the user input
-func _change_velocity():
-	var direction_x = Input.get_axis("ui_left", "ui_right")
-	if direction_x:
-		velocity.x = direction_x * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+func _change_velocity(delta):
+	# gets the input vector (horizontal_input, vertical_input)
+	var input := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# change the size of this vector to 1 to limit the diagonal movement speed
+	input = input.normalized()
 	
-	var direction_y = Input.get_axis("ui_up", "ui_down")
-	if direction_y:
-		velocity.y = direction_y * speed
+	# if you press any keys you accelerate until reaching the max speed 
+	if input:
+		velocity = velocity.move_toward(input * speed, acceleration * delta)
 	else:
-		velocity.y = move_toward(velocity.x, 0, speed)
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 
 func _physics_process(delta):
-	_change_velocity()
+	_change_velocity(delta)
 	move_and_slide()
