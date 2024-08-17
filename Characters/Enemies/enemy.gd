a7ef5d52625625cc5e0ab1c4f99f0f35405dfe33
Beginercod <Beginercod@users.noreply.github.com>
2024-08-17 12:28:54 +0200

    Fixed enemy movement

--- Characters/Enemies/enemy.gd
+++ Characters/Enemies/enemy.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
-@export var speed = 100
-var player_pos
-var direction
-@onready var player = get_node("../Player")
+@export var speed = 250 ##we will change it later
+@onready var player = get_node("../Player") ##finds player
 
 func _physics_process(delta):
-	player_pos = player.global_position
-	direction= global_position.direction_to(player_pos)
-	position += direction * speed * delta
-	look_at(player_pos)
+	var direction = global_position.direction_to(player.global_position) ##gets the direction
+	position += direction * speed * delta ##moves enemy
+	look_at(player.global_position) ##looks at player
 	move_and_slide()
+
+
+
