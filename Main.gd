0fe37b2919b7c0eafde40f3da807f65c7536a101
ItayElf <thh123321@gmail.com>
2024-08-17 21:31:33 +0300

    Fix: removed dead code

--- Main.gd
+++ Main.gd
@@ -1,13 +1,6 @@
 extends Node2D
 @onready var path_follow = $Path2D/PathFollow2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 func spawn_enemy():
 	var new_enemy = null
 	var enemytype = -1
