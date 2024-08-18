df5090e6c110da3a4445f8ef69fb282dc96bc9b9
endevour97 <thegreatendeavour97@gmail.com>
2024-08-18 09:19:56 +0200

    Added system for enemy pathfinding

--- Characters/Enemies/enemy.gd
+++ Characters/Enemies/enemy.gd
@@ -5,19 +5,32 @@ extends CharacterBody2D
 @export var speed: float = 340.
 @export var acceleration: float = 500.0
 
+var is_navigation_ready = false
+var current_target
 var player
 var health:
 	set = set_health
 
-@onready var health_bar = $"Health Bar"
+@onready var health_bar : ProgressBar = $"Health Bar"
+@onready var navigation : NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	health = max_health
+	
+	current_target = player
+	
+	# Magic code from the documentation
+	navigation.path_desired_distance = 4.0
+	navigation.target_desired_distance = 4.0
+	
+	call_deferred("actor_setup")
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
-	velocity = velocity.move_toward(direction * speed, acceleration * delta)
+	if is_navigation_ready:
+		if not navigation.is_navigation_finished():
+			var direction = global_position.direction_to(navigation.get_next_path_position())
+			velocity = velocity.move_toward(direction * speed, acceleration * delta)
 	move_and_slide()
 
 func get_hit(dmg):
@@ -29,3 +42,12 @@ func set_health(value):
 	health = value
 	health_bar.value = health / max_health
 	
+
+func actor_setup():
+	await get_tree().physics_frame
+	is_navigation_ready = true
+	navigation.target_position = current_target.position
+
+
+func _on_navigation_resfresh_timeout():
+	navigation.target_position = current_target.position
